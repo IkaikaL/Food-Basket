e8f2779858e9221db18261a92f1b8d168f599560
LSU-blara4 <60485940+LSU-blara4@users.noreply.github.com>
2021-11-06 20:42:57 -0500

    Building recipelist

--- basket/lib/main.dart
+++ basket/lib/main.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -7,15 +8,23 @@ import 'src/settings/settings_service.dart';
 // Page Imports
 import 'pages/samplePage.dart';
 
+//Database Imports
+import 'package:basket/database/app_database.dart';
+import 'package:basket/database/recipe.dart';
+
+Future<List<Recipe>> recipeList = List.empty() as Future<List<Recipe>>;
+
 void main() {
   print("main.dart is running");
+  recipeList = AppDatabase.instance.readAllRecipes();
+
   runApp(const BasketApp());
 }
 
 // Launches the app but does not display a screen
 class BasketApp extends StatelessWidget {
   const BasketApp({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -33,8 +42,6 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
-  final _biggerFont = const TextStyle(fontSize: 28.0);
-  String username = "UserName";
   int index = 0;
 
   @override
@@ -50,13 +57,13 @@ class _LandingScreenState extends State<LandingScreen> {
               title: const Text("I am a test string"),
               children: [
                 ListTile(
-                  leading: Icon(Icons.arrow_drop_down_circle),
                   title: const Text('Recipe Title'),
                   subtitle: Text(
                     'Secondary Text',
                     style: TextStyle(color: Colors.black.withOpacity(0.6)),
                   ),
                 ),
+                 
               ],
             ),
           );
