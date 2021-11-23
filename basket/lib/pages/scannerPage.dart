1fd5227ac4f0f74f11849a2e4b2889d9ebb6aba9
AdamDupuy <ajdupuy15@gmail.com>
2021-11-23 11:26:04 -0600

    committing some changes to try to fix this shit
    fuck

--- basket/lib/pages/scannerPage.dart
+++ basket/lib/pages/scannerPage.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: file_names
-
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 import 'package:flutter/material.dart';
 
 class BarCodeScanner extends StatefulWidget {
@@ -12,12 +12,22 @@ class BarCodeScanner extends StatefulWidget {
 }
 
 class BarCodeState extends State<BarCodeScanner> {
-  String _data = "";
+  String? _data;
 
-  Future<String> _scan() async {
-    return FlutterBarcodeScanner.scanBarcode(
+  Future _scan() async {
+    /*return FlutterBarcodeScanner.scanBarcode(
             "#000000", "Cancel", true, ScanMode.BARCODE)
-        .then((value) => setState(() => _data = value) as String);
+        .then((value) => setState(() => _data = value) as String); */
+    String _data;
+    try {
+      _data = (await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', false, ScanMode.BARCODE));
+    } on PlatformException {
+      _data = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() => this._data = _data);
   }
 
   /* @override
@@ -28,60 +38,77 @@ class BarCodeState extends State<BarCodeScanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Scanner'), centerTitle: true),
-        body:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          TextButton(
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(16.0),
-                primary: Colors.white,
-                textStyle: const TextStyle(fontSize: 20),
+      appBar: AppBar(title: const Text('Groceries Scanner'), centerTitle: true),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue[500],
+                  onPrimary: Colors.black,
+                ),
+                icon: Icon(Icons.camera_alt_outlined),
+                label: Text('StartScan'),
+                onPressed: _scan,
               ),
-              onPressed: () =>
-                  _scan(), //async => setState(() => _data = _scan() as String)
-              child: const Text('Start Scan')),
-          Text(_data)
-        ]),
-        floatingActionButton: ElevatedButton.icon(
-          onPressed: () => _scan(),
-          icon: const Icon(Icons.add),
-          label: const Text('Start Scan'),
-          //Text(_data) //commented this and changed from 44 down
-        ));
+              SizedBox(height: 20),
+              Text(
+                _data == null
+                    ? 'Select the button below to start scanning Groceries.'
+                    : 'Scan Result: $_data',
+                style: TextStyle(fontSize: 22),
+              )
+            ]),
+      ),
+      /*floatingActionButton: ElevatedButton.icon(
+        icon: const Icon(Icons.add),
+        label: const Text('Start Scan'),
+        onPressed: () => _scan(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,*/
+    );
   }
 }
 
 /*
-floatingActionButton: ElevatedButton.icon(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const addItemToInventoryManually()));
-            refreshInventory();
-            build(context);
-          },
-          icon: const Icon(Icons.add),
-          label: const Text('add items'),
-        )
+  String _data = "1";
 
+  Future<String> _scan() async {
+    /*return FlutterBarcodeScanner.scanBarcode(
+            "#000000", "Cancel", true, ScanMode.BARCODE)
+        .then((value) => setState(() => _data = value) as String); */
+    String barcodeScanRes;
 
+    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666', 'Cancel', false, ScanMode.BARCODE);
+    print(barcodeScanRes);
+    _data = barcodeScanRes;
+    return barcodeScanRes;
+  }
 
+  /* @override
+  void initState() {
+    super.initState();
+  }*/
 
-
-
-
-
-body:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          TextButton(
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(16.0),
-                primary: Colors.white,
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              onPressed: () =>
-                  _scan(), //async => setState(() => _data = _scan() as String)
-              child: const Text('Start Scan')),
-          Text(_data)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Groceries Scanner'), centerTitle: true),
+      body: const Center(
+        child: Text(
+          'Select the button below to start scanning Groceries.',
+          style: TextStyle(fontSize: 22),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      floatingActionButton: ElevatedButton.icon(
+        icon: const Icon(Icons.add),
+        label: const Text('Start Scan'),
+        onPressed: () => _scan(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
           */
