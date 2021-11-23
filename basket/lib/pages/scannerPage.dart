9d182f97554e26b9f276eafdad5d4af35577b1fe
Ikaika <thebestsurfer11@gmail.com>
2021-11-22 21:47:56 -0600

    telling file to ignore shit

--- basket/lib/pages/scannerPage.dart
+++ basket/lib/pages/scannerPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter/material.dart';
